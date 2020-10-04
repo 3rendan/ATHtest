abb1eec701c308cd4f91b5c067d10081e182a17f
3rendan <brendan.ryan@pm.me>
2020-10-04 14:38:37 -0400

    working on creating multiple API calls to get items and images

--- src/components/Items.js
+++ src/components/Items.js
@@ -2,46 +2,32 @@ import React, { Component } from 'react'
 import axios from 'axios';
 
 class Items extends Component {
-  constructor(props) {
-    super(props)
+  state = {
+      items: [],
+      images: []
+  };
 
-    this.state = {
-      items: []
-    }
-  }
-  componentDidMount() {
-    axios('http://digital.provath.org/api/items')
-    .then(res => {
-      this.setState({ items: res.data })
-    })
-    .catch(error => console.error(error))
-  }
 
 
   render() {
-    const { items } = this.state;
-    
-    console.log(this.state.data);
+    function getData () {
+      axios.all([
+        axios.get('http://digital.provath.org/api/items'),
+        axios.get('http://digital.provath.org/api/files')
+      ])
+      .then(res => {
+        this.setState({ items: res[0].data, images: res[1].data })
+      })
+      .catch(error => console.error(error))
+    }
+    getData();
+    console.log('hello')
+
 
 
     return (
       <div>
-      { items.map(item => {
-
-        console.log()
-        return  (
-            <div key={item.id} className="text-center card-4">
-                <small> {item.element_texts[0].element.name} : {item.element_texts[0].text}</small>
-                <small> {item.element_texts[1].element.name} : {item.element_texts[1].text}</small>
-                <small> {item.element_texts[2].element.name} : {item.element_texts[2].text}</small>
-                <small> {item.element_texts[3].element.name} : {item.element_texts[3].text}</small>
-                <small> {item.element_texts[4].element.name} : {item.element_texts[4].text}</small>
-                <small> {item.element_texts[5].element.name} : {item.element_texts[5].text}</small>
-                <small> {item.element_texts[6].element.name} : {item.element_texts[6].text}</small>
-
-            </div>
-          )
-      })}
+        <h1>hello</h1>
       </div>
     )
   }
