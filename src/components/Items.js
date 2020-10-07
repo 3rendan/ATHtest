a5e9141e887c44c0b26198009fc34304ebd1a1ec
3rendan <brendan.ryan@pm.me>
2020-10-07 16:32:20 -0400

    working on getting items aligned with images. can hardcode an id now, cannot figure out how to pass item.id into src= for images array

--- src/components/Items.js
+++ src/components/Items.js
@@ -1,36 +1,44 @@
 import React, { Component } from 'react'
 import axios from 'axios';
 
-class Items extends Component {
+export default class Items extends Component {
   state = {
       items: [],
       images: []
-  };
+  }
+  componentDidMount(){
+    axios.all([
+      axios.get('http://digital.provath.org/api/items'),
+      axios.get('http://digital.provath.org/api/files')
+    ])
+    .then(res => {
+      this.setState({ items: res[0].data, images: res[1].data })
+    })
+    .catch(error => console.error(error))
+  }
+render(){
+  const { images, items } = this.state;
+  return (
+    <div>
+      { items.map(item => {
+        return  (
+            <div key={item.id} className="text-center card">
+              <h1>{item.id}</h1>
+               {console.log(item.id)}
+                <small> {item.element_texts[0].element.name} : {item.element_texts[0].text}</small>
+                <small> {item.element_texts[1].element.name} : {item.element_texts[1].text}</small>
+                <small> {item.element_texts[2].element.name} : {item.element_texts[2].text}</small>
+                <small> {item.element_texts[3].element.name} : {item.element_texts[3].text}</small>
+                <small> {item.element_texts[4].element.name} : {item.element_texts[4].text}</small>
+                <small> {item.element_texts[5].element.name} : {item.element_texts[5].text}</small>
+                <small> {item.element_texts[13].element.name} : {item.element_texts[13].text}</small>
+                <img src={images[`${item.id}`].file_urls.thumbnail} alt={item.element_texts[0].text} />
 
 
-
-  render() {
-    function getData () {
-      axios.all([
-        axios.get('http://digital.provath.org/api/items'),
-        axios.get('http://digital.provath.org/api/files')
-      ])
-      .then(res => {
-        this.setState({ items: res[0].data, images: res[1].data })
-      })
-      .catch(error => console.error(error))
-    }
-    getData();
-    console.log('hello')
-
-
-
-    return (
-      <div>
-        <h1>hello</h1>
+            </div>
+          )})
+        }
       </div>
     )
   }
 }
-
-export default Items
