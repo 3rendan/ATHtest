243189c598f388266ea506d0951e259dca41aca5
3rendan <brendan.ryan@pm.me>
2020-10-07 21:05:45 -0400

    styling the Item.js with flexbox, Item.js now has dummy fields

---
+++ src/components/Item.js
@@ -0,0 +1,41 @@
+import React, { Component } from 'react'
+import axios from 'axios';
+
+export default class Items extends Component {
+  state = {
+      items: [],
+      images: []
+  }
+  componentDidMount(){
+    axios.all([
+      axios.get('http://digital.provath.org/api/items'),
+      axios.get('http://digital.provath.org/api/files')
+    ])
+    .then(res => {
+      this.setState({ items: res[0].data, images: res[1].data })
+    })
+    .catch(error => console.error(error))
+  }
+render(){
+  const { images, items } = this.state;
+  // using id as param passed from items.js link rather than Map function
+  return (
+    <div>
+      <!-- using id as param passed from items.js link rather than Map function -->
+        return  (
+            <div key={item.id} className="text-center card">
+              <h1>{item.id}</h1>
+               {console.log(item.id)}
+               <img src={images[29].file_urls.thumbnail} alt={item.element_texts[0].text} />
+                <h4> {item.element_texts[0].element.name} : {item.element_texts[0].text}</h4>
+                <h4> {item.element_texts[1].element.name} : {item.element_texts[1].text}</h4>
+                <h4> {item.element_texts[2].element.name} : {item.element_texts[2].text}</h4>
+                <h4> {item.element_texts[3].element.name} : {item.element_texts[3].text}</h4>
+                <h4> {item.element_texts[4].element.name} : {item.element_texts[4].text}</h4>
+                <h4> {item.element_texts[5].element.name} : {item.element_texts[5].text}</h4>
+                <h4> {item.element_texts[13].element.name} : {item.element_texts[13].text}</h4>
+
+
+
+            </div>
+          )})
--- src/components/Items.js
+++ src/components/Items.js
@@ -16,25 +16,32 @@ export default class Items extends Component {
     })
     .catch(error => console.error(error))
   }
+
 render(){
   const { images, items } = this.state;
+  const imgBrowse = {
+    maxWidth: '100%',
+    margin: 'auto'
+  };
+  const cardBrowse = {
+    maxWidth: '200px',
+    margin: 'auto',
+    border: '1px #000 solid',
+    float: 'left',
+    padding: '10px',
+  };
   return (
     <div>
       { items.map(item => {
         return  (
-            <div key={item.id} className="text-center card">
-              <h1>{item.id}</h1>
-               {console.log(item.id)}
-                <small> {item.element_texts[0].element.name} : {item.element_texts[0].text}</small>
-                <small> {item.element_texts[1].element.name} : {item.element_texts[1].text}</small>
-                <small> {item.element_texts[2].element.name} : {item.element_texts[2].text}</small>
-                <small> {item.element_texts[3].element.name} : {item.element_texts[3].text}</small>
-                <small> {item.element_texts[4].element.name} : {item.element_texts[4].text}</small>
-                <small> {item.element_texts[5].element.name} : {item.element_texts[5].text}</small>
-                <small> {item.element_texts[13].element.name} : {item.element_texts[13].text}</small>
-                <img src={images[`${item.id}`].file_urls.thumbnail} alt={item.element_texts[0].text} />
-
-
+            <div key={item.id} className="card-3" style={cardBrowse}>
+            <div className="row" style="flex-container">
+             <img style={imgBrowse} src={images[29].file_urls.thumbnail} alt={item.element_texts[0].text} />
+             <div className="card-body text-center">
+               <h3>{item.element_texts[0].text}</h3>
+               <small> {item.element_texts[5].element.name} : {item.element_texts[5].text}</small>
+              </div>
+              </div>
             </div>
           )})
         }
