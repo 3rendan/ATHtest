284933b5c125a6c1d9f5529663a80647c725b184
3rendan <brendan.ryan@pm.me>
2020-10-26 12:13:12 -0400

    got item info to display on Item.js by adding .data to res in Calls.js

--- src/Calls.js
+++ src/Calls.js
@@ -10,14 +10,14 @@ import axios from 'axios';
 //   })
 // }
 
-export const useItem = (param) => {
-  console.log(param)
+const useItem = (param) => {
   const [item, setItem] = useState([]);
   useEffect(() =>{
     axios.get(`http://digital.provath.org/api/items/` + param)  //if you use a num with corresponding id an object is returned
-      .then(res => setItem(res))
+      .then(res => setItem(res.data))
       .catch(error => console.error(error))
     }, [])
-    console.log(item)
+    // console.log(item)
     return item;
   }
+export default useItem;
--- src/views/Item.js
+++ src/views/Item.js
@@ -1,16 +1,19 @@
 import React, { useEffect, useState } from 'react';
 import { Link } from "react-router-dom";
-import axios from 'axios'
+import axios from 'axios';
 
-import { useItem } from '../Calls'
+import useItem  from '../Calls'
 
   const Item = (item) => {
-    console.log(item.match.params.id)
-    const singleItem = useItem(item.match.params.id);
-    console.log(singleItem.data)
+    let x = useItem(item.match.params.id);
+    console.log(x)
+    console.log(x.id)
+
+
     return (
+
       <div>
-        <h1>how do I pass item props</h1>
+        <h1>{x.id}</h1>
       </div>
     )
   }
