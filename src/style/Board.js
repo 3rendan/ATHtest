3ef8d6e19402f241b7d587b5f056127fe0f4cc1c
3rendan <brendan.ryan@pm.me>
2021-09-22 15:07:02 -0400

    fixed spacing on browse and collections by coverting to flex

--- src/style/Board.js
+++ src/style/Board.js
@@ -2,12 +2,12 @@ import styled from 'styled-components';
 
 
 const Board = styled.div`
-  display: grid;
-  grid-template-columns: 1fr 1fr 1fr;
-  grid-gap: .5rem;
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: space-around;
   color: black;
   padding: .5rem 0 2rem 0;
-  align-content: space-evenly;
+
 
   @media only screen and (max-width: 768px) {
     display: grid;
--- src/style/CardBrowse.js
+++ src/style/CardBrowse.js
@@ -3,7 +3,9 @@ import styled from 'styled-components';
 
 const CardBrowse = styled.div`
   text-align: center;
+  display: inline-block;
   min-height: 33vh;
+  max-width: 33%;
   border-top: 1.5rem #999999 solid;
   border-radius: 5px 5px 0 0;
   @media only screen and (max-width: 600px) {
