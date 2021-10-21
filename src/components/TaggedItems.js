b97fdfdc610321401530247c24e90a3f07543251
3rendan <brendan.ryan@pm.me>
2021-10-21 10:50:42 -0400

    comment out TaggedItems.js

--- src/components/TaggedItems.js
+++ src/components/TaggedItems.js
@@ -1,28 +1,28 @@
-import React from 'react'
-import useFetch from '../services/useFetch'
-import Spinner from '../services/Spinner'
-
-const TaggedItems = (props) => {
-  const { data: items, loading, error } = useFetch('items');
-  const { data: images } = useFetch('files');
-  //const id = parseInt(props.match.params.id)
-
-
-
-  if (error) throw error;
-  if (loading) return <Spinner />;
-  return (
-    <>
-      { items.flatMap((item) =>{
-        let tagId = props.tagId
-        item.tags.flatMap((e) =>{
-          console.log(e)
-        })
-        })
-        }
-    </>
-  )
-}
-
-
-export default TaggedItems
+// import React from 'react'
+// import useFetch from '../services/useFetch'
+// import Spinner from '../services/Spinner'
+//
+// const TaggedItems = (props) => {
+//   const { data: items, loading, error } = useFetch('items');
+//   const { data: images } = useFetch('files');
+//   //const id = parseInt(props.match.params.id)
+//
+//
+//
+//   if (error) throw error;
+//   if (loading) return <Spinner />;
+//   return (
+//     <>
+//       { items.flatMap((item) =>{
+//         let tagId = props.tagId
+//         item.tags.flatMap((e) =>{
+//           console.log(e)
+//         })
+//         })
+//         }
+//     </>
+//   )
+// }
+//
+//
+// export default TaggedItems
