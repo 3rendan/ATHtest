a794f23b73760bebaf074ba43c9cee60225cafbc
3rendan <brendan.ryan@pm.me>
2021-10-21 11:08:11 -0400

    modal on mobile image

--- src/views/Item.js
+++ src/views/Item.js
@@ -28,7 +28,22 @@ const Item = (props) => {
         <h3>{ item.element_texts[0].text }</h3>
         <h5>{ getCollection(item.collection.id) }</h5>
       </section>
-      <MobileImg  item={ item } images={ images }/>
+      <button className="img-modal" onClick={()=> setIsOpen(true) }><MobileImg  item={ item } images={ images }/></button>
+      <Modal open={ isOpen } onClose={ () => setIsOpen(false)} >
+        { images.map((image) => {
+          if(image.item.id === item.id){
+            return (
+                <img
+                className='img-modal'
+                src={ image.file_urls.fullsize }
+                alt={item.element_texts[0].text}
+                image={ image.file_urls.fullsize }/>
+              )
+            }
+            return null
+          })
+          }
+      </Modal>
       <TombstoneImg> {/* this will house image and tombstone in grid */}
         <Tombstone item={ item }/>
         <button className="img-modal" onClick={()=> setIsOpen(true) }><ItemImg item={ item } images={ images }/></button>
