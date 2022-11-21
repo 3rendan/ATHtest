dac79868f5d97ca6484d92431d83f682de85482c
Brendan Ryan <brendan.ryan@pm.me>
2022-11-20 22:55:23 -0500

    Hook created for useItemImage

--- src/components/singleItem/SingleItem.js
+++ src/components/singleItem/SingleItem.js
@@ -2,14 +2,20 @@ import React from 'react'
 import useItem from '../../hooks/useItem'
 import getCollection from '../../services/getCollection'
 import Bars from '../../services/Bars'
+import ProgressBar from '../../services/ProgressBar'
 import { useParams } from 'react-router-dom'
+import useItemImage from '../../hooks/useItemImage'
 
 
 const SingleItem = () =>  {
     const { id } = useParams()
     const res = useItem(id)
+    const imgRes = useItemImage(id)
     const item = res.item
+    const image = imgRes.image
+    console.log(imgRes)
     if (res.loading) return <Bars />
+    if (imgRes.loading1) return <ProgressBar />
 
     return (
         <div>
--- src/hooks/useItemImage.js
+++ src/hooks/useItemImage.js
@@ -1,8 +1,28 @@
-import React, { useState, useEffect } from 'react'
-import ItemsContext from '../context/ItemsContext'
+import { useEffect, useState } from "react"
+import axios from 'axios'
 
-const useItemImage = (itemId) => {
-    const [ image, setImage ] = useContext(ItemsContext)
-    images.map(image => image.item.id === props.item.id)
+function useItemImage(id) {
+    const [ loading1, setLoading1 ] = useState(true)
+    const [ image, setImage ] = useState([])
+    const [ error, setError ] = useState(null)
+
+    useEffect(() =>{
+        const getImage = async () => {
+            try {
+                const res = await axios.get(`https://digital.provath.org/api/files`);
+                const images = await res.data
+                // This returns an object when id is hardcoded
+                const imageObject = await images.find(image => image.item.id === id)
+                setImage(imageObject)
+                setLoading1(false)
+            } catch (err) {
+                setError(err)
+                setLoading1(false)
+            }
+        }
+        getImage()
+        // eslint-disable-next-line react-hooks/exhaustive-deps
+    },[]) 
+    return { image, loading1, error }
 }
 export default useItemImage
