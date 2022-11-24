8a508308db861e1a3327070b0e42a9b88881bfaa
Brendan Ryan <brendan.ryan@pm.me>
2022-11-23 21:40:36 -0500

    Updated changes in views/Item.js

--- src/views/Item.js
+++ src/views/Item.js
@@ -39,55 +39,29 @@ const Item = () =>  {
     if (isLoading) return <Bars />
     
     return (
-      <div className='container'>
-      <section>  {/* title and collection row */}
-        <h3>{ item.element_texts[0].text }</h3>
-        <h5>{ getCollection(item.collection.id) }</h5>
-      </section>
-      <button className="img-modal" onClick={()=> setIsOpen(true) }><MobileImg  item={ item } images={ images }/></button>
-      <Modal open={ isOpen } onClose={ () => setIsOpen(false)} >
-        { images.map((image) => {
-          if(image.item.id === item.id){
-            return (
-                <img
-                className='img-modal'
-                src={ image.file_urls.fullsize }
-                alt={item.element_texts[0].text}
-                image={ image.file_urls.fullsize }/>
-              )
-            }
-            return null
-          })
-          }
-      </Modal>
-      <TombstoneImg> {/* this will house image and tombstone in grid */}
-        <Tombstone item={ item }/>
-        <button className="img-modal" onClick={()=> setIsOpen(true) }><ItemImg item={ item } images={ images }/></button>
-        <Modal open={ isOpen } onClose={ () => setIsOpen(false)} >
-          { images.map((image) => {
-            if(image.item.id === item.id){
-              return (
-                  <img
-                  className='img-modal'
-                  src={ image.file_urls.fullsize }
-                  alt={item.element_texts[0].text}
-                  image={ image.file_urls.fullsize }/>
-                )
-              }
-              return null
-            })
-            }
-        </Modal>
-      </TombstoneImg>
-      <div className='socials'>
-        <h1>Tags</h1>
-        <Socials />
-      </div>
-      <TagsDwnlds>
-        <ItemTags item={ item }/>
-        <Dwnlds item={ item }/>
-      </TagsDwnlds>
-      </div>
+    <>
+    {console.log(archiveItem)}
+        <ItemTitle item={archiveItem}/>
+        {/* Image that appears on mobile devices */}
+        <MobileImg title={archiveItem.element_texts[0].text} />
+        <div className="row">
+            <div className="col-7">
+                <Tombstone />
+            </div>
+            <div className="col-5">
+                <ItemImg title={archiveItem.element_texts[0].text}/>
+            </div>
+        </div>
+        <div className="row">
+            <div className="col-7">
+                <ItemTags item={archiveItem} />
+            </div>
+            <div className="col-5">
+                <Socials />
+                <Dwnlds item={archiveItem} /> 
+            </div>
+        </div>
+    </>
     )
 }
 export default Item
